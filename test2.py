ebd0d0982f1a373b22cc6c44733e0f53acab4f0a
ZahraBeheshti <zt.beheshti@gmail.com>
2022-05-26 19:30:04 +0430

    add three files for test algebra

---
+++ test2.py
@@ -0,0 +1,26 @@
+from numpy import array
+from numpy.linalg import qr
+
+A = array([
+[1, 2],
+[3, 4],
+[5, 6]])
+print(A)
+# calculate transpose
+C = A.T
+print(C)
+
+
+# define rectangular matrix
+A = array([
+[1, 2],
+[3, 4],
+[5, 6]])
+print(A)
+# factorize
+Q, R = qr(A, 'complete')
+print(Q)
+print(R)
+# reconstruct
+B = Q.dot(R)
+print(B)
